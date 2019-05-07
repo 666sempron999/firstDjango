567ffa977e73f221c5f2c54f6d69a3e6d3a3f82e
Yaroslav Rushechnicov <yrushechnicov@activewizards.com>
2019-05-07 18:57:20 +0300

    Add CRUD test for author

--- library/tests.py
+++ library/tests.py
@@ -7,11 +7,34 @@ from .models import Author, Book
 class AuthorModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
+        """
+        create test
+        """
         #Set up non-modified objects used by all test methods
         Author.objects.create(first_name='Aleksandr', last_name='Lenon')
 
     def test_first_name_label(self):
+        """
+        read test
+        """
         author=Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label,'first name')
+
+    def test_first_name_update(self):
+        """
+        update first_name test
+        """
+        author=Author.objects.get(id=1)
+        Author.objects.update(first_name='Aleksandr1')
+
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'first name')
+
+
+    def test_first_name_delete(self):
+        author = Author.objects.get(id=1)
+        author.delete()
         field_label = author._meta.get_field('first_name').verbose_name
         self.assertEquals(field_label,'first name')
 
@@ -19,16 +42,18 @@ class AuthorModelTest(TestCase):
 # class BookModelTest(TestCase):
 #     @classmethod
 #     def setUpTestData(cls):
-#         Author.objects.create(first_name='Aleksandr', last_name='Lenon')
-#         print("==========")
-#         print(dir(Author))
-#         print(str(Author.id))
-#         print("==========")
-#         Book.objects.create(author=1, title='Home', description='Home home', year_of_creating=1999)
-
+#         author = Author.objects.create(first_name='Aleksandr', last_name='Lenon')
+#         Book.objects.create(author=author, title='Home', description='Home home', year_of_creating=1999)
 
 
 #     def test_title_label(self):
 #         title=Book.objects.get()
 #         field_label = title._meta.get_field('title').verbose_name
 #         self.assertEquals(field_label,'title')
+
+#     def test_title_update(self):
+#         pass
+
+#     def test_title_delete(self):
+#         pass
+
